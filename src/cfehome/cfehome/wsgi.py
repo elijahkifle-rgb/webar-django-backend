039fec461f7bdcd164ef7684b042833f6d958c66
elijahkifle <elijahkifle@gmail.com>
2026-03-05 16:19:27 +0100

    Add wsgi diagnostic print

--- src/cfehome/cfehome/wsgi.py
+++ src/cfehome/cfehome/wsgi.py
@@ -6,7 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
+import sys
+print("WSGI.PY IS LOADING – APP IS STARTING", file=sys.stderr)
 import os
 
 from django.core.wsgi import get_wsgi_application
