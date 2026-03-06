8d5e6ba3a4bca956252e62b832221296099bfa31
elijahkifle <elijahkifle@gmail.com>
2026-03-06 16:13:07 +0100

    Add diagnostic prints in scores app

--- scores/urls.py
+++ scores/urls.py
@@ -1,8 +1,9 @@
+import sys
+print("Loading scores/urls.py", file=sys.stderr)
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-        path('test/', views.test_api, name='test_api'),
+    path('test/', views.test_api, name='test_api'),
     path('live/<int:match_id>/', views.live_score, name='live_score'),
 ]
--- scores/views.py
+++ scores/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+import sys
+print("Loading scores/views.py", file=sys.stderr)
 from django.http import JsonResponse
 
 def test_api(request):
