6de7023b2613a492fbe805373b15cbe3fc76798b
elijahkifle <elijahkifle@gmail.com>
2026-03-07 14:26:01 +0100

    Add diagnostic prints and ping endpoint

--- src/cfehome/cfehome/urls.py
+++ src/cfehome/cfehome/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import JsonResponse
 
 def ping(request):
@@ -8,4 +8,5 @@ def ping(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ping/', ping),
+    path('api/', include('scores.urls')),
 ]
