afd8efdc031b66c2fe169a214730be9f69927013
elijahkifle <elijahkifle@gmail.com>
2026-03-08 15:16:16 +0100

    Add missing live_score view

--- scores/views.py
+++ scores/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
 import sys
 print("Loading scores/views.py", file=sys.stderr)
 from django.http import JsonResponse
+
+def live_score(request, match_id):
+    return JsonResponse({"match_id": match_id, "message": "Live score endpoint hit"})
 
 def test_api(request):
     return JsonResponse({'message': 'API is working'})
